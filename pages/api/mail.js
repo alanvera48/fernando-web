ecc6bace2daf4a528914c96cbd676834ed93a3c7
alanvera48@gmail.com <alanvera48@gmail.com>
2021-11-16 03:20:49 -0300

    fix

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,5 +1,4 @@
 export default (req, res) => {
   const body = JSON.parse(req.body);
-  (body);
   res.status(200).json({ status: "Ok" });
 };
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import routes from "../src/constant";
 export default function Home() {
   return (
     <div>
-      {routes.map((el) => {
-        return <Containers element={el}></Containers>;
+      {routes.map((el, index) => {
+        return <Containers element={el} key={index}></Containers>;
       })}
     </div>
   );
